a934291ced9994df4d36da18803074750ec25bd1
dasun-dev <dasamelbdev@gmail.com>
2024-12-24 17:56:45 +1100

    adding updated utility functions

--- py-server-main/util/error_handling_util.py
+++ py-server-main/util/error_handling_util.py
@@ -1,28 +1,41 @@
-import logging
-from typing import Tuple
+"""
+error_handling_util.py
 
+This module provides utility functions for error handling
+"""
 from util.validation_util import validate_string_argument, validate_http_status_code
 
 
-def build_error_response(error_message: str, status_code: int) -> tuple[str, int]:
+def build_http_error_response(error_message: str, status_code: int) -> tuple[str, int]:
     """
-        Constructs an error response with the provided error message and HTTP status code.
+    Constructs an HTTP error response with the provided error message and status code.
 
-        This function validates that `error_message` is a non-empty string and `status_code`
-        is a valid HTTP status code. It logs the error message for tracking purposes.
+    This function validates that the `error_message` is a non-empty string and the `status_code`
+    is a valid HTTP status code. It returns a tuple containing the error message and status code,
+    suitable for use in HTTP responses. The function also logs the error message for tracking purposes.
 
-        Args:
-            error_message (str): The error message to include in the response.
-            status_code (int): The HTTP status code for the error response.
+    Args:
+        error_message (str): The error message to include in the HTTP response.
+        status_code (int): The HTTP status code to associate with the error response.
 
-        Returns:
-            tuple[str, int]: A tuple containing the error message and status code.
+    Returns:
+        tuple[str, int]: A tuple containing the error message and status code.
 
-        Raises:
-            ValueError: If `error_message` is not a valid string or `status_code` is not a
-                        valid HTTP status code.
-        """
+    Raises:
+        TypeError: If `error_message` is not a string or `status_code` is not an integer.
+        ValueError: If `error_message` is an empty or whitespace-only string, or
+                    `status_code` is not a valid HTTP status code.
+
+    Example:
+        >>> build_http_error_response("Bad Request", 400)
+        ('Bad Request', 400)
+
+        >>> build_http_error_response("", 400)
+        ValueError: The provided value is an empty or whitespace-only string
+
+        >>> build_http_error_response("Bad Request", 999)
+        ValueError: The provided status code 999 is not a valid HTTP status code
+    """
     validate_string_argument(error_message)
     validate_http_status_code(status_code)
-    logging.info(f"{error_message}")
     return error_message, status_code
